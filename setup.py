8de2c9306c5ee5a188d4a6d81983c334a97f21f5
Kushal Das <kushaldas@gmail.com>
2016-01-04 21:24:03 +0530

    Updates to 0.6

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup, Extension
 
 module1 = Extension('elfdata',
                     include_dirs = ['/usr/include/elfutils','.'],
-                    libraries = ['elf','asm','dw','ebl'],
-                    #library_dirs = ['/usr/lib/mysql','/usr/lib64/mysql'],
+                    libraries = ['elf','dw'],
                     sources = ['elfdata.c'],
                     extra_compile_args=['-std=c99'])
 
 setup (name = 'elfdata',
-       version = '0.5',
+       version = '0.6',
        description = 'elfdata from elf files',
        author = 'Kushal Das',
        author_email = 'kushaldas@gmail.com',
